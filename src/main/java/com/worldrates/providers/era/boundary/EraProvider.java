ffb7eb8e60252c35850d77c82c425cfbe28c1399
matt <matt.matea@gmail.com>
2020-02-13 21:48:32 +0200

    add exchange rate api

---
+++ src/main/java/com/worldrates/providers/era/boundary/EraProvider.java
@@ -0,0 +1,53 @@
+package com.worldrates.providers.era.boundary;
+
+import com.worldrates.app.boundary.RatesProvider;
+import com.worldrates.app.entity.ExchangeRate;
+import com.worldrates.providers.bnr.entity.DataSet;
+import com.worldrates.providers.era.entity.EraRates;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@ConditionalOnProperty("app.providers.era.enabled")
+public class EraProvider implements RatesProvider {
+
+
+    private final String url;
+    private final RestTemplate rest;
+
+    public EraProvider(@Value("${app.providers.era.url}") String url, RestTemplate rest) {
+        this.url = url;
+        this.rest = rest;
+    }
+
+    @Override
+    public String getId() {
+        return "ERA";
+    }
+
+    @Override
+    public List<ExchangeRate> getDailyRates() {
+        EraRates eraRates = rest.getForObject(url, EraRates.class);
+
+      //todo: date from json & base currency
+
+       return eraRates.getRates().entrySet().stream().map(entry -> new ExchangeRate(
+                null,
+                getId(),
+                "EUR",
+                BigDecimal.ONE,
+                entry.getKey(),
+                entry.getValue(),
+                LocalDate.now(),
+                LocalDateTime.now()
+        )).collect(Collectors.toList());
+    }
+}
